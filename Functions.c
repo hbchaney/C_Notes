3a287a308edfe4a524b70ce0a8c02a1a12743b8e
hbchaney <hbchaney@gmail.com>
2021-08-19 12:49:03 -0400

    commit

--- Functions.c
+++ Functions.c
@@ -16,11 +16,17 @@ functions stop running after going through all the lines of code that it has
 for functions to return a value they must out put a return statement 
 
 return also works to break out of the function if no output is stated 
+
+the main function will always run first while the created function will fun when it is called 
+
+
+variables within functions cannot be referenced outside of functions 
 */
 
 #include <stdio.h>
 
 float babiesfirst(int); //will inform the complier of the function and its o/i types before it is used
+//not necessary if the function is writen above the main function
 
 int main() 
 {
