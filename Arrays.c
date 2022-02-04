6f836a68de0fd60af15f1938a6e0123a2a39f4ce
hbchaney <hbchaney@gmail.com>
2022-02-03 20:40:02 -0500

    commit

--- Arrays.c
+++ Arrays.c
@@ -15,7 +15,7 @@ number of elements in an array cannot be changed after the array is declared
 
 int main() 
 {
-    int salaries_list[] = {1,2,3,4}; //dont forget the brackets
+    int salaries_list[4] = {1,2,3,4}; //dont forget the brackets
     int x; 
     int total = 0; 
 
@@ -23,7 +23,7 @@ int main()
     for (x=0; x<4; x++)
     {
         total = salaries_list[x] + total;
-        printf("%d\n",total);
+        printf("%d ",total);
 
     }
 }
